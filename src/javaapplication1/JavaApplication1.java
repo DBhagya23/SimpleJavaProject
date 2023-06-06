f7248a5704bafa68d80fb51aee955c14368e1f63
DBhagya23 <dasunbhagyapremathilaka@gmail.com>
2023-06-06 06:36:35 +0530

    Create Login program

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author dasun
+ */
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+        int[]numbers = {2,4,6,8};
+        String[] names = new String[5];
+        names[0] = "kamal" ;
+        names[1] = "passan";
+        System.out.println(names[4]);
+        int[][] any = {{1,2,3},
+                       {4,5,6},
+                       {7,8,9}
+        };
+    System.out.println(any[1][0]);
+    }
+    
+}
